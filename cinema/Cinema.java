9d93d0f7b7dcfc6312a3c0d4f2ee94a13f601d16
Mustafa <Ycesan>
2024-09-26 07:09:15 +0200

    set up seating format

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -3,6 +3,23 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
+        System.out.println("Cinema:");
 
+        int rows = 7;
+        int cols = 8;
+
+        System.out.print("  ");
+        for (int i = 1; i <= cols; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= cols; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
     }
 }
