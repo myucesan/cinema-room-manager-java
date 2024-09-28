72bab45fd37e63447ea3a4aa29a88ef26f4ed127
Mustafa <Ycesan>
2024-09-28 10:44:49 +0200

    removed unneccesary printRoom function (based on requirements).
    Added menu

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -1,32 +1,61 @@
 package cinema;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
     static final Integer priceFront = 10;
     static final Integer priceBack = 8;
+    static Scanner input = new Scanner(System.in);
+    static String[][] room;
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
-        int rows = input.hasNextInt() ? input.nextInt(): 0;
+        int rows = input.hasNextInt() ? input.nextInt() : 0;
         System.out.println("Enter the number of seats in each row:");
         int cols = input.hasNextInt() ? input.nextInt() : 0;
+        room = new String[rows + 1][cols + 1];
+        for (String[] row : room) {
+            Arrays.fill(row, "S");  // Fill each row with "S"
+        }
 
-        printRoom(rows, cols);
-        System.out.println("Enter a row number:");
-        int chosenRow = input.hasNextInt() ? input.nextInt() : 0;
-        System.out.println("Enter a seat number in that row:");
-        int chosenSeat = input.hasNextInt() ? input.nextInt() : 0;
-
-        System.out.println("$" + calculateTicketPrice(rows, cols, chosenRow));
-        printRoom(rows, cols, chosenRow, chosenSeat);
-
-
-
+        while (!printMenu()) ;
+        return;
     }
 
-    static int calculateTicketPrice(int rows, int cols, int chosenRow) {
+    private static boolean purchaseTicket(int chosenRow, int chosenSeat) {
+        if (room[chosenRow][chosenSeat].equals("S")) {
+            room[chosenRow][chosenSeat] = "B";
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean printMenu() {
+        System.out.println("1. Show the seats");
+        System.out.println("2. Buy a ticket");
+        System.out.println("0. Exit");
+        int choice = input.hasNextInt() ? input.nextInt() : 0;
+        if (choice == 1) {
+            printRoom();
+            return false;
+        } else if (choice == 2) {
+            System.out.println("Enter a row number:");
+            int chosenRow = input.hasNextInt() ? input.nextInt() : 0;
+            System.out.println("Enter a seat number in that row:");
+            int chosenSeat = input.hasNextInt() ? input.nextInt() : 0;
+            if (!purchaseTicket(chosenRow, chosenSeat)) {
+                // handle when ticket is already bought
+            }
+            System.out.println("$" + calculateTicketPrice(chosenRow));
+            return false;
+        } else return choice == 0;
+    }
+
+
+    static int calculateTicketPrice(int chosenRow) {
+        int rows = room.length - 1;
+        int cols = room[0].length - 1;
         int totalSeats = rows * cols;
         System.out.println("Ticket price:");
         if (totalSeats <= 60) {
@@ -40,8 +69,10 @@ public class Cinema {
         }
     }
 
-    static void printRoom(int rows, int cols, int chosenRow, int chosenSeat) {
+    static void printRoom() {
         System.out.println("Cinema:");
+        int rows = room.length - 1;
+        int cols = room[0].length - 1;
         // notice that the top row does not need to be part of a loop
         System.out.print("  ");
         for (int i = 1; i <= cols; i++) {
@@ -52,27 +83,9 @@ public class Cinema {
         for (int i = 1; i <= rows; i++) {
             System.out.print(i);
             for (int j = 1; j <= cols; j++) {
-                if (i == chosenRow && j == chosenSeat) {
-                    System.out.print(" B");
-                } else {
-                    System.out.print(" S");
-                }
+                System.out.print(" " + room[i][j]);
             }
             System.out.println();
         }
     }
-
-    static void printRoom(int rows, int cols) {
-        System.out.println("Cinema:");
-        System.out.print("  ");
-        for (int i = 1; i <= cols; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        for (int i = 1; i <= rows; i++) {
-            System.out.print(i + " S".repeat(cols));
-            System.out.println();
-        }
     }
-}
